d779cc7b209bb37df8710d4e2905eafaaf3efc6f
Anupamgautam22 <119593744+Anupamgautam22@users.noreply.github.com>
2023-10-11 11:22:42 +0545

    edit

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -27,6 +27,10 @@ namespace BulkyBookWeb.Controllers
 		[ValidateAntiForgeryToken]
 		public IActionResult Create(Category obj)
 		{
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactily match the name.");
+            }
             if (ModelState.IsValid)
             {
                 _db.Categories.Add(obj);
@@ -34,6 +38,37 @@ namespace BulkyBookWeb.Controllers
                 return RedirectToAction("Index");
             }
             return View(obj);
+		}
+
+		public IActionResult Edit(int? id)
+		{
+            if (id == null || id== 0) {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+			return View(categoryFromDb);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Edit(Category obj)
+		{
+			if (obj.Name == obj.DisplayOrder.ToString())
+			{
+				ModelState.AddModelError("name", "The DisplayOrder cannot exactily match the name.");
+			}
+			if (ModelState.IsValid)
+			{
+				_db.Categories.Add(obj);
+				_db.SaveChanges();
+				return RedirectToAction("Index");
+			}
+			return View(obj);
 		}
 	}
 }
