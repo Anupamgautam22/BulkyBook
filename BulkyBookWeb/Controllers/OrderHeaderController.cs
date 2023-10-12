67b2139eb1d12a2b1e3af5c83d7bc7b3ddb805f2
Anupamgautam22 <119593744+Anupamgautam22@users.noreply.github.com>
2023-10-12 14:01:03 +0545

    changes in db context for order

---
+++ BulkyBookWeb/Controllers/OrderHeaderController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class OrderHeaderController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderHeaderController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: OrderHeader
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.OrderHeader.Include(o => o.ApplicationUser);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: OrderHeader/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.OrderHeader == null)
+            {
+                return NotFound();
+            }
+
+            var orderHeader = await _context.OrderHeader
+                .Include(o => o.ApplicationUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (orderHeader == null)
+            {
+                return NotFound();
+            }
+
+            return View(orderHeader);
+        }
+
+        // GET: OrderHeader/Create
+        public IActionResult Create()
+        {
+            ViewData["ApplicationUserId"] = new SelectList(_context.Set<ApplicationUser>(), "Id", "Id");
+            return View();
+        }
+
+        // POST: OrderHeader/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ApplicationUserId,OrderDate,ShippingDate,OrderTotal,OrderStatus,PaymentStatus,TrackingNumber,Carrier,PaymentDate,PaymentDueDate,SessionId,PaymentIntentId,PhoneNumber,StreetAddress,City,State,PostalCode,Name")] OrderHeader orderHeader)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(orderHeader);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ApplicationUserId"] = new SelectList(_context.Set<ApplicationUser>(), "Id", "Id", orderHeader.ApplicationUserId);
+            return View(orderHeader);
+        }
+
+        // GET: OrderHeader/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.OrderHeader == null)
+            {
+                return NotFound();
+            }
+
+            var orderHeader = await _context.OrderHeader.FindAsync(id);
+            if (orderHeader == null)
+            {
+                return NotFound();
+            }
+            ViewData["ApplicationUserId"] = new SelectList(_context.Set<ApplicationUser>(), "Id", "Id", orderHeader.ApplicationUserId);
+            return View(orderHeader);
+        }
+
+        // POST: OrderHeader/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ApplicationUserId,OrderDate,ShippingDate,OrderTotal,OrderStatus,PaymentStatus,TrackingNumber,Carrier,PaymentDate,PaymentDueDate,SessionId,PaymentIntentId,PhoneNumber,StreetAddress,City,State,PostalCode,Name")] OrderHeader orderHeader)
+        {
+            if (id != orderHeader.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(orderHeader);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderHeaderExists(orderHeader.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ApplicationUserId"] = new SelectList(_context.Set<ApplicationUser>(), "Id", "Id", orderHeader.ApplicationUserId);
+            return View(orderHeader);
+        }
+
+        // GET: OrderHeader/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.OrderHeader == null)
+            {
+                return NotFound();
+            }
+
+            var orderHeader = await _context.OrderHeader
+                .Include(o => o.ApplicationUser)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (orderHeader == null)
+            {
+                return NotFound();
+            }
+
+            return View(orderHeader);
+        }
+
+        // POST: OrderHeader/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.OrderHeader == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.OrderHeader'  is null.");
+            }
+            var orderHeader = await _context.OrderHeader.FindAsync(id);
+            if (orderHeader != null)
+            {
+                _context.OrderHeader.Remove(orderHeader);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrderHeaderExists(int id)
+        {
+          return (_context.OrderHeader?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
--- BulkyBookWeb/Data/ApplicationDbContext.cs
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -14,6 +14,8 @@ namespace BulkyBookWeb.Data
         public DbSet<BulkyBookWeb.Models.Company>? Company { get; set; }
         public DbSet<BulkyBookWeb.Models.Product>? Product { get; set; }
         public DbSet<BulkyBookWeb.Models.CoverType>? CoverType { get; set; }
+        public DbSet<BulkyBookWeb.Models.OrderDetail>? OrderDetail { get; set; }
+        public DbSet<BulkyBookWeb.Models.OrderHeader>? OrderHeader { get; set; }
      
         
        
